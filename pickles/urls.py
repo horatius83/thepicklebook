308ebce0adaa5852ef3b6b04b4541aff14c5395e
horatius83 <horatius83@gmail.com>
2022-03-22 06:10:01 -0500

    Added stubs for review and pickle detail pages

--- pickles/urls.py
+++ pickles/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('review/<int:review_id>', views.review, name='review'),
+    path('review/new', views.review_new, name='new review'),
+    path('<int:pickle_id>', views.pickle, name='pickle'),
+    path('new', views.pickle_new, name='new pickle')
 ]
--- pickles/views.py
+++ pickles/views.py
@@ -3,3 +3,15 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("PICKLES!")
+
+def review(request, review_id):
+    return HttpResponse(f"Looking at review {review_id}")
+
+def review_new(request):
+    return HttpResponse("Looking at new review page")
+
+def pickle(request, pickle_id):
+    return HttpResponse(f'Looking at pickle {pickle_id}')
+
+def pickle_new(request):
+    return HttpResponse(f'Looking at new pickle page')
