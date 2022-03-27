215c0e1b3172dfa02c4a4afe559dc2b14211f48b
horatius83 <horatius83@gmail.com>
2022-03-26 22:21:05 -0500

    Added some templates and ability to add new pickles

--- pickles/urls.py
+++ pickles/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('review/<int:review_id>', views.review, name='review'),
     path('review/new', views.review_new, name='new review'),
     path('<int:pickle_id>', views.pickle, name='pickle'),
-    path('new', views.pickle_new, name='new pickle')
+    path('new', views.pickle_new, name='new pickle'),
+    path('manufacturer', views.pickle_maker_new, name="new pickle maker"),
+    path('manufacturer/<int:pickle_maker_id>', views.pickle_maker, name="pickle maker"),
+    path('manufactuer/all', views.pickle_maker_all, name='all pickle makers')
 ]
--- pickles/views.py
+++ pickles/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pickle, PickleMaker, Tag
+from django.shortcuts import get_object_or_404, render
+import json
 
 def index(request):
     return HttpResponse("PICKLES!")
@@ -10,8 +13,36 @@ def review(request, review_id):
 def review_new(request):
     return HttpResponse("Looking at new review page")
 
+def pickle_maker(request, pickle_maker_id):
+    return HttpResponse(f"Looking at pickle maker {pickle_maker_id}")
+
+def pickle_maker_new(request):
+    return HttpResponse('Looking at new pickle maker page')
+
+def pickle_maker_all(request):
+    makers = PickleMaker.objects.all()
+    return json.dumps(sorted(m.name for m in makers))
+
 def pickle(request, pickle_id):
-    return HttpResponse(f'Looking at pickle {pickle_id}')
+    pickle = get_object_or_404(Pickle, pk=pickle_id)
+    return render(request, 'pickles/pickle.html', {'pickle': pickle})
 
 def pickle_new(request):
-    return HttpResponse(f'Looking at new pickle page')
+    if request.POST:
+        name = request.POST['new_pickle_name']
+        maker = request.POST['pickle_maker']
+        tags = request.POST['pickle_tags']
+        pickle_makers = PickleMaker.objects.filter(name=maker)
+        if len(pickle_makers) > 0:
+            pickle_maker = pickle_makers[0]
+            pickles = Pickle.objects.filter(name=name)
+            if len(pickles) == 0:
+                new_pickle = Pickle(name=name, maker=pickle_maker)
+                new_pickle.save()
+                return pickle(request, new_pickle.id)
+            else:
+                return pickle(request, pickles[0].id)
+        else:
+            return pickle_maker_new(request)
+    else:
+        return render(request, 'pickles/new_pickle.html')  
